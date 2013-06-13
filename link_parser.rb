3a8e25b8443875905ecaa9b70d7d49c812f0b2ae
karottenreibe <git@rottenrei.be>
2013-06-13 09:11:34 +0200

    fixed link parser

--- link_parser.rb
+++ link_parser.rb
@@ -2,7 +2,7 @@ class LinkParser
 
   # Extracts all interesting links from a snippet of text.
   def parse(text)
-    return text.scan(/^(?:https?\:\/\/|www.)[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(?:\/\S*)?$/).uniq
+    return text.scan(/(?:https?\:\/\/|www.)[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(?:\/\S*)?/).uniq
   end
 
 end
